2ce4bb2c0cb69983f5b338c2b9d365c2d1b8bb8c
Maksym Konotop <konotop2001@gmail.com>
2020-01-23 20:27:10 +0200

    Реализованны методы нормализации и масштабирования. Изменен интерфейс метода Learn

--- NeuralNetworksTests/NeuralNetworkTests.cs
+++ NeuralNetworksTests/NeuralNetworkTests.cs
@@ -14,32 +14,35 @@ namespace NeuralNetworks.Tests
         [TestMethod()]
         public void FeedForwardTest()
         {
-            var dataset = new List<Tuple<double, double[]>>
+            var outputs = new double[] { 0, 0, 1, 0, 0 };
+            var inputs = new double[,]
             {
-                new Tuple<double, double[]> (0,new double[] {0,0,0,0}),
-                new Tuple<double, double[]> (0,new double[] {0,0,0,1}),
-                new Tuple<double, double[]> (1,new double[] {0,0,1,0}),
-                new Tuple<double, double[]> (0,new double[] {0,0,1,1}),
-                new Tuple<double, double[]> (0,new double[] {0,1,0,0}),
+               {0,0,0,0},
+               {0,0,0,1},
+               {0,0,1,0},
+               {0,0,1,1},
+               {0,1,0,0},
 
             };
 
-            var topology = new Topology(4, 1,0.1, 2);
+            var topology = new Topology(4, 1, 0.1, 2);
             var neuralNetwork = new NeuralNetwork(topology);
-            var difference = neuralNetwork.Learn(dataset, 100000);
+            var difference = neuralNetwork.Learn(outputs,inputs, 10000);
 
             var results = new List<double>();
-            foreach (var data in dataset)
+            for (int i = 0; i < outputs.Length; i++)
             {
-                var res = neuralNetwork.FeedForward(data.Item2).Output;
+                var row = NeuralNetwork.GetRow(inputs, i);
+                var res = neuralNetwork.FeedForward(row).Output;
                 results.Add(res);
             }
+          
 
             for (int i = 0; i < results.Count; i++)
             {
-                var expected = Math.Round(dataset[i].Item1,3);
-                var actual = Math.Round(results[i], 3);
-                Assert.AreEqual(expected,actual);
+                var expected = Math.Round(outputs[i], 2);
+                var actual = Math.Round(results[i], 2);
+                Assert.AreEqual(expected, actual);
             }
 
         }
